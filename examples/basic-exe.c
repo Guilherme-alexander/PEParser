240784780cdf87b8aa8b455ee1f50460d5fe3dd6
Guilherme Alexander <80430814+Guilherme-alexander@users.noreply.github.com>
2026-06-28 21:03:39 -0300

    Format main function and improve indentation

--- examples/basic-exe.c
+++ examples/basic-exe.c
@@ -1,11 +1,13 @@
+// (MinGW GCC)
 // BUILD: gcc basic-exe.c -o basic-exe.exe
+
+// (Visual Studio)
 // BUILD: cl basic-exe.c /Fe:basic.exe
 
 #include <stdio.h>
 
+
 int main(){
-
- printf("[+]EXECUTE! EXE PE\n");
-
- return 0;
+    printf("[+]EXECUTE! EXE PE\n");
+    return 0;
 }
